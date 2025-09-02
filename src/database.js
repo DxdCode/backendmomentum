fc297047829bc09ce54245491149591e6fcda656
DxdCode <davidmuela2407@gmail.com>
2025-09-01 20:23:37 -0500

    Inicializar

---
+++ src/database.js
@@ -0,0 +1,11 @@
+import {Client} from 'pg';
+
+const client = new Client({
+  user: 'database-user',
+  password: 'secretpassword!!',
+  host: 'my.database-server.com',
+  port: 5334,
+  database: 'database-name',
+})
+
+export default client;
