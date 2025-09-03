0638801b5fa4ba4470a018c6fc270654be76e1f4
DxdCode <davidmuela2407@gmail.com>
2025-09-03 14:10:20 -0500

    -

--- src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import UserController from "../controllers/UserController.js";
-import { protect } from "../middlewares/auth.middleware.js";
-
-const router = Router();
-router.get("/profile", protect, UserController.getProfile);
-router.put("/profile", protect, UserController.updateProfile);
-
-export default router;
